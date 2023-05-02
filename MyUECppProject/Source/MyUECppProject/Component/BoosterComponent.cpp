e6f16914f0db69312c7add821bf402417fc2fca1
nutti <nutti.metro@gmail.com>
2023-05-02 16:16:23 +0900

    Add: License body to the source codes

--- MyUECppProject/Source/MyUECppProject/Component/BoosterComponent.cpp
+++ MyUECppProject/Source/MyUECppProject/Component/BoosterComponent.cpp
@@ -1,4 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*!
+ * MyUECppProject
+ *
+ * Copyright (c) 2023 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
 #include "BoosterComponent.h"
 
