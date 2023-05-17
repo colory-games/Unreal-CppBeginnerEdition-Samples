6a32dcddda7d98e8832268dab1423c5230c96259
beisaku <beisaku1toriaezu@gmail.com>
2023-05-18 00:24:11 +0900

    fix

--- MyUECppProject/Source/MyUECppProject/BPFunctionLibrary/MathLibrary.cpp
+++ MyUECppProject/Source/MyUECppProject/BPFunctionLibrary/MathLibrary.cpp
@@ -20,6 +20,25 @@ double UMathLibrary::Add(double A, double B)
 	return C;
 }
 
+void UMathLibrary::Divide(int32 A, int32 B, int32& Quotient, int32& Remainder)
+{
+	Quotient = A / B;
+	Remainder = A % B;
+}
+
+void UMathLibrary::AddRef(UPARAM(ref) double& A, double B)
+{
+	A = A + B;
+}
+
+double UMathLibrary::AddCompact(double A, double B)
+{
+	double C;
+
+	C = A + B;
+	return C;
+}
+
 double UMathLibrary::Clamp(double Value, double Min, double Max)
 {
 	if (Value < Min)
@@ -51,38 +70,10 @@ int32 UMathLibrary::Accumulate(int32 N)
 	return Out;
 }
 
-void UMathLibrary::Divide(int32 A, int32 B, int32& Quotient, int32& Remainder)
-{
-	Quotient = A / B;
-	Remainder = A % B;
-}
 
-void UMathLibrary::AddRef(UPARAM(ref) double& A, double B)
-{
-	A = A + B;
-}
 
 void UMathLibrary::MinMax(double A, double B, double C, double& Min, double& Max)
 {
 	Min = UKismetMathLibrary::FMin(UKismetMathLibrary::FMin(A, B), C);
 	Max = UKismetMathLibrary::FMax(UKismetMathLibrary::FMax(A, B), C);
-}
-
-double UMathLibrary::ClampCompact(double Value, double Min, double Max)
-{
-	if (Value < Min)
-	{
-		return Min;
-	}
-	else
-	{
-		if (Value > Max)
-		{
-			return Max;
-		}
-		else
-		{
-			return Value;
-		}
-	}
 }
