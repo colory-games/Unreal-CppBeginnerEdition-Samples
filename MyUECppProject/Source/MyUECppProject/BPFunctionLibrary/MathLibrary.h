245e677f1bb56ee8bd7f5292901e2d3d1fb7df21
beisaku <98695390+beisaku@users.noreply.github.com>
2023-05-19 18:22:49 +0900

    Fix: FuntionLibrary sample (#12)
    Co-authored-by: nutti <nutti.metro@gmail.com>

--- MyUECppProject/Source/MyUECppProject/BPFunctionLibrary/MathLibrary.h
+++ MyUECppProject/Source/MyUECppProject/BPFunctionLibrary/MathLibrary.h
@@ -27,20 +27,20 @@ public:
 	static double Add(double A, double B);
 
 	UFUNCTION(BlueprintCallable)
-	static double Clamp(double Value, double Min, double Max);
-
-	UFUNCTION(BlueprintCallable)
-	static int32 Accumulate(int32 N);
-
-	UFUNCTION(BlueprintCallable)
 	static void Divide(int32 A, int32 B, int32& Quotient, int32& Remainder);
 
 	UFUNCTION(BlueprintCallable)
 	static void AddRef(UPARAM(ref) double& A, double B);
 
-	UFUNCTION(BlueprintCallable)
-	static void MinMax(double A, double B, double C, double& Min, double& Max);
+	UFUNCTION(BlueprintPure, Category = "Math", meta = (CompactNodeTitle = "+"))
+	static double AddCompact(double A, double B);
 
-	UFUNCTION(BlueprintPure, Category = "Math", meta = (CompactNodeTitle = "Clamp"))
-	static double ClampCompact(double Value, double Min, double Max);
+	UFUNCTION(BlueprintPure, Category = "Math")
+	static double Clamp(double Value, double Min, double Max);
+
+	UFUNCTION(BlueprintPure, Category = "Math")
+	static int32 Accumulate(int32 N);
+
+	UFUNCTION(BlueprintPure, Category = "Math", meta = (CompactNodeTitle = "MinMax"))
+	static void MinMax(double A, double B, double C, double& Min, double& Max);
 };
